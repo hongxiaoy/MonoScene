096e103e3e7f0a9a306bb2173e66b1a242e25bae
Hongxiao Yu <112234465+hongxiaoy@users.noreply.github.com>
2023-06-25 14:38:15 +0800

    Update fusion.py

--- monoscene/data/utils/fusion.py
+++ monoscene/data/utils/fusion.py
@@ -418,7 +418,7 @@ class TSDFVolume:
 
 
 def rigid_transform(xyz, transform):
-    """Applies a rigid transform to an (N, 3) pointcloud."""
+    """对一个 (N, 3) 点云应用一个刚体变换."""
     xyz_h = np.hstack([xyz, np.ones((len(xyz), 1), dtype=np.float32)])
     xyz_t_h = np.dot(transform, xyz_h.T).T
     return xyz_t_h[:, :3]
